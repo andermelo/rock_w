b39344f9490c4f9fe5792f0838753b53c72e6a7b
Anderson <anderson@gafa.com.br>
2023-11-04 19:34:08 -0300

    New pacakges and dependency
    - Installing new packages
    - registering dependency class

---
+++ lib/core/api/api_const.dart
@@ -0,0 +1,1 @@
+const String apiUrl = 'https://api.openweathermap.org/data/2.5/';
---
+++ lib/dependencies/register_dependencies.dart
@@ -0,0 +1,5 @@
+import 'package:get_it/get_it.dart';
+
+class RegisterDependencies {
+  static on(GetIt getIt) {}
+}
